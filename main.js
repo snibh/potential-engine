6e10ae2b37c721da4d8af92698e092151ce72d34
snibh <76776218+snibh@users.noreply.github.com>
2021-09-07 12:18:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = imageClassifier('MobileNet',modelLoaded);
+}
+function draw() {
+
+image(video,0,0,300,300);
+classifier.classify(video,gotResult);
+
+}
+
+function modelLoaded() {
+
+console.log("modelLoaded");
+
+}
+function preload() {
+
+
+
+}
+function gotResult(error,results) {
+
+if(error) 
+{
+  console.error(error);
+}
+ else 
+{
+  console.log(results);
+  document.getElementById("Object").innerHTML = results[0].label;
+  document.getElementById("Accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+
+}
+
+
+
+
